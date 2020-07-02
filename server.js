a3a1eaad5324d3cfef5d99b83345bfd93cd63f0f
Chris-Fortier <vendor@chrisjfortier.com>
2020-07-01 17:02:26 -0700

    prepare for deploy

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 // res means response
 
 // need one of these for every url route
 app.use("/api/v1/users", require("./api/v1/users")); // the route and then the file
 app.use("/api/v1/memory-cards", require("./api/v1/memory-cards")); // the route and then the file
-app.get("/", (req, res) => res.send("Hello World!"));
+
+// if none of the routes are hit, use what's in the build folder
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+   // "*" is a wildcard
+   res.sendFile(path.resolve(__dirname, "client", "build", "index.html")); // __dirname means present directory
+});
 
 const port = process.env.PORT || 3045; // use the variable we have for the port or a default port of 3045
 app.listen(port, () =>
